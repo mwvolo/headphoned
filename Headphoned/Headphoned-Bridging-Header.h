b02f4bf632548b259b2dfdecaafbb9eb2f621301
Michael Harrison <mwharrison@rice.edu>
2015-09-15 21:00:15 -0500

    add objc bridge and ezaudio

---
+++ Headphoned/Headphoned-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Headphoned-Bridging-Header.h
+//  Headphoned
+//
+//  Created by Michael Harrison on 9/15/15.
+//  Copyright (c) 2015 Michael Harrison. All rights reserved.
+//
+
+#ifndef Headphoned_Headphoned_Bridging_Header_h
+#define Headphoned_Headphoned_Bridging_Header_h
+
+#import "EZAudio/EZAudio.h"
+
+#endif
